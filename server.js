87e7f06149b7b448d52b7b7ba123c2418a1a9aa2
dgasch512 <gaschdav208@gmail.com>
2020-09-29 20:44:14 -0500

    port update

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.get('/profile/:id', (req,res) => { profile.handleProfileGet(req, res, db)});
 app.put('/image', (req,res) => { image.handleImage(req, res, db)});
 
 
-app.listen(port, () => {
+app.listen(process.env.DATABASE_URL, () => {
   console.log(`App running on port: ${port}`)
 });
